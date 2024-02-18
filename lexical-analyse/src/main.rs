ff015eea19b938da1d2366c3bbf6140f9246aad8
Skvortsov A.V <skvorcovav@oblgas.by>
2024-02-18 23:57:04 +0300

    lexical analyse upd

--- lexical-analyse/src/main.rs
+++ lexical-analyse/src/main.rs
@@ -1,115 +1,219 @@
 use std::str::Chars;
 
 #[derive(Debug, PartialEq)]
-enum TokenType {
-    Keyword,
-    Identifier,
-    Number,
-    Symbol,
-    Whitespace,
-    Unknown,
+enum Token {
+    Keyword(String),
+    Identifier(String),
+    Operator(String),
+    Integer(i32),
+    Float(f64),
+    StringLiteral(String),
+    Indent,
+    Dedent,
+    Newline,
 }
 
-#[derive(Debug)]
-struct Token {
-    token_type: TokenType,
-    lexeme: String,
+struct Lexer<'a> {
+    input: Chars<'a>,
+    current_char: Option<char>,
 }
 
-fn is_keyword(token: &str) -> bool {
-    let keywords = vec![
-        "fn", "if", "else", "while", "for", "in", "print", "true", "false", "None",
-    ];
-    keywords.contains(&token)
-}
-
-fn is_identifier(token: &str) -> bool {
-    token.chars().next().unwrap().is_alphabetic()
-}
-
-fn is_number(token: &str) -> bool {
-    token.parse::<f64>().is_ok()
-}
-
-fn tokenize(code: &str) -> Vec<Token> {
-    let mut tokens = Vec::new();
-    let mut chars = code.chars().peekable();
-    let mut current_token = String::new();
-
-    while let Some(ch) = chars.next() {
-        if ch.is_whitespace() {
-            if !current_token.is_empty() {
-                tokens.push(Token {
-                    token_type: determine_token_type(&current_token),
-                    lexeme: current_token.clone(),
-                });
-                current_token.clear();
-            }
-            continue;
-        }
-
-        if ch.is_ascii_punctuation() {
-            if !current_token.is_empty() {
-                tokens.push(Token {
-                    token_type: determine_token_type(&current_token),
-                    lexeme: current_token.clone(),
-                });
-                current_token.clear();
-            }
-            tokens.push(Token {
-                token_type: TokenType::Symbol,
-                lexeme: ch.to_string(),
-            });
-            continue;
-        }
-
-        if ch.is_ascii_alphabetic() || ch == '_' {
-            let mut lookahead = *chars.peek().unwrap_or(&'\0');
-            if lookahead.is_alphanumeric() || lookahead == '_' {
-                current_token.push(ch);
-                continue;
-            }
-        }
-
-        current_token.push(ch);
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Lexer<'a> {
+        let mut lexer = Lexer {
+            input: input.chars(),
+            current_char: None,
+        };
+        lexer.advance();
+        lexer
     }
 
-    if !current_token.is_empty() {
-        tokens.push(Token {
-            token_type: determine_token_type(&current_token),
-            lexeme: current_token,
-        });
+    fn advance(&mut self) {
+        self.current_char = self.input.next();
     }
 
-    tokens
-}
+    fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        let mut indentation_stack = Vec::new();
+        let mut current_indentation = 0;
 
-fn determine_token_type(token: &str) -> TokenType {
-    if is_keyword(token) {
-        TokenType::Keyword
-    } else if is_identifier(token) {
-        TokenType::Identifier
-    } else if is_number(token) {
-        TokenType::Number
-    } else if token.chars().all(|ch| ch.is_whitespace()) {
-        TokenType::Whitespace
-    } else {
-        TokenType::Unknown
+        loop {
+                match self.current_char {
+                Some(' ') => {
+                    self.advance();
+                }
+                Some('\n') => {
+                    self.advance();
+                    tokens.push(Token::Newline);
+                    let mut spaces_count = 0;
+                    while let Some(' ') = self.current_char {
+                        self.advance();
+                        spaces_count += 1;
+                    }
+                    if spaces_count > current_indentation {
+                        tokens.push(Token::Indent);
+                        indentation_stack.push(current_indentation);
+                        current_indentation = spaces_count;
+                    } else {
+                        while spaces_count < current_indentation {
+                            tokens.push(Token::Dedent);
+                            current_indentation = indentation_stack.pop().unwrap();
+                        }
+                    }
+                }
+                Some(ch) => {
+                    if ch.is_ascii_digit() || ch == '-' {
+                        tokens.push(self.consume_number());
+                    } else if ch.is_alphabetic() {
+                        tokens.push(self.consume_identifier());
+                    } else {
+                        match ch {
+                            '#' => {
+                                self.advance();
+                                self.consume_comment();
+                            }
+                            '"' => {
+                                self.advance();
+                                tokens.push(self.consume_string_literal());
+                            }
+                            _ => {
+                                tokens.push(self.consume_operator());
+                            }
+                        }
+                    }
+                }
+                None => break,
+            }
+        }
+
+        while current_indentation > 0 {
+            tokens.push(Token::Dedent);
+            current_indentation = indentation_stack.pop().unwrap();
+        }
+
+        tokens
+    }
+
+    fn consume_number(&mut self) -> Token {
+        let mut num_str = String::new();
+        let mut is_float = false;
+        let mut is_negative = false;
+
+        if let Some(ch) = self.current_char {
+            if ch == '-' {
+                is_negative = true;
+                self.advance();
+            }
+        }
+
+        while let Some(ch) = self.current_char {
+            if ch.is_ascii_digit() {
+                num_str.push(ch);
+                self.advance();
+            } else if ch == '.' && !is_float {
+                num_str.push(ch);
+                self.advance();
+                is_float = true;
+            } else {
+                break;
+            }
+        }
+
+        if is_float {
+            if let Ok(f) = num_str.parse::<f64>() {
+                if is_negative {
+                    Token::Float(-f)
+                } else {
+                    Token::Float(f)
+                }
+            } else {
+                panic!("Invalid float number: {}", num_str);
+            }
+        } else {
+            if let Ok(i) = num_str.parse::<i32>() {
+                if is_negative {
+                    Token::Integer(-i)
+                } else {
+                    Token::Integer(i)
+                }
+            } else {
+                panic!("Invalid integer: {}", num_str);
+            }
+        }
+    }
+
+    fn consume_identifier(&mut self) -> Token {
+        let mut identifier = String::new();
+        while let Some(ch) = self.current_char {
+            if ch.is_alphanumeric() || ch == '_' {
+                identifier.push(ch);
+                self.advance();
+            } else {
+                break;
+            }
+        }
+
+        match identifier.as_str() {
+            "if" | "else" | "for" | "while" | "def" | "class" => Token::Keyword(identifier),
+            _ => Token::Identifier(identifier),
+        }
+    }
+
+    fn consume_comment(&mut self) {
+        while let Some(ch) = self.current_char {
+            if ch == '\n' {
+                break;
+            }
+            self.advance();
+        }
+    }
+
+    fn consume_string_literal(&mut self) -> Token {
+        let mut literal = String::new();
+        let mut is_escaped = false;
+
+        while let Some(ch) = self.current_char {
+            if is_escaped {
+                literal.push(ch);
+                is_escaped = false;
+            } else if ch == '\\' {
+                is_escaped = true;
+            } else if ch == '"' {
+                self.advance();
+                break;
+            } else {
+                literal.push(ch);
+            }
+            self.advance();
+        }
+
+        Token::StringLiteral(literal)
+    }
+
+    fn consume_operator(&mut self) -> Token {
+        let mut operator = String::new();
+        while let Some(ch) = self.current_char {
+            if ch.is_ascii_whitespace() || ch.is_alphanumeric() {
+                break;
+            }
+            operator.push(ch);
+            self.advance();
+        }
+        Token::Operator(operator)
     }
 }
 
 fn main() {
-    let code = r#"
-       a = 5
-       b: int = -43
-       def hello_world():
-            print('Hello, World!')
-
-            hello_world()
+    let input = r#"
+        x = -5
+        if x < 10:
+            print("Hello, world!")
     "#;
 
-    let tokens = tokenize(code);
+    let mut lexer = Lexer::new(input);
+    let tokens = lexer.tokenize();
+
     for token in tokens {
-        println!("{:?}: {}", token.token_type, token.lexeme);
+        println!("{:?}", token);
     }
 }
