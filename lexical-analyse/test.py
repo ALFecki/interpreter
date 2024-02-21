da9f3381e4ecf7a2d4f953fafebb6931336d8a60
Skvortsov A.V <skvorcovav@oblgas.by>
2024-02-21 13:00:26 +0300

    updated test data

--- lexical-analyse/test.py
+++ lexical-analyse/test.py
@@ -1,9 +1,41 @@
-x = -5
-k = 10
+# Переменные и операции
+x = 5
+y = -3
+z = 1e9
+f = 4.324234
+= 50
+print(z)
 
-e = 1e8
+# Условные операторы
+if x > y:
+    print("x больше y")
+elif x < y:
+    print("x меньше y")
+else:
+    print("x равно y")
 
-k += 10
+# Циклы
+for i in range(5):
+    print(i)
 
-if x < 10:
-    print("Hello, world!")
+while x > 0:
+        print(x)
+    x *= 1
+
+# Функции
+def add(a, b):
+    try:
+        return a + b
+        try:
+            pass
+        except:
+            pass
+
+result = add(2, 3)
+print(result)
+
+# Строки
+string = "Hello, world!"
+print(string[0])
+print(string.upper())
+
