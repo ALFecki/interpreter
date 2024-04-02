3e615e858ff15a96ef5d9630addc9f4fa49f3d07
ALFecki <alexskvr03@gmail.com>
2024-04-02 23:55:52 +0300

    test python file

---
+++ syntax-analyzer/test.py
@@ -0,0 +1,40 @@
+# Переменные и операции
+x = 5
+y = -3
+z = 1e9
+f = 4.324234
+f = 50
+print(z)
+
+# Условные операторы
+if x > y:
+    print("x больше y")
+elif x < y:
+    print("x меньше y")
+else:
+    print("x равно y")
+
+# Циклы
+for i in range(5):
+    print(i)
+
+while x > 0:
+    print(x)
+    x *= 1
+
+# Функции
+def add(a, b):
+    try:
+        return a + b
+
+    except:
+        pass
+
+result = add(2, 3)
+print(result)
+
+# Строки
+string = "Hello, world!"
+print(string[0])
+print(string.upper())
+
