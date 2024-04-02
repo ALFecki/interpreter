22d15f8ccf70a4cda742e4f31edc2abbf20f538b
ALFecki <alexskvr03@gmail.com>
2024-04-03 01:19:17 +0300

    parsing test added

--- syntax-analyzer/parser_test/src/main.rs
+++ syntax-analyzer/parser_test/src/main.rs
@@ -6,11 +6,11 @@ fn main() {
     let mut file = File::open("./test.py").expect("Unable to open the file");
     let mut python_source = String::new();
     file.read_to_string(&mut python_source).expect("Unable to read the file");
-    // let ast = ast::Suite::parse("", "./test.py");
-    let python_statements = ast::Suite::parse(python_source.as_str(), "<test>").unwrap();  // statements
-    // let python_expr = ast::Expr::parse(python_source, "<embedded>").unwrap();  // or expr
-    println!("{:#?}", python_statements);
 
+    let python_statements = ast::Suite::parse_without_path(python_source.as_str());
 
-    // assert!(ast.is_ok());
+    match python_statements {
+        Ok(stmts) => {println!("{:#?}", stmts);},
+        Err(e) => { println!("ERROR IN PARSING: {}", e)}
+    }
 }
